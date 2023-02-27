d54bed24eb53a2deb5cecdc55c9d6c1ab6698647
Juan Becerra <jdb9056@nyu.edu>
2023-02-27 15:51:52 -0500

    Commit heap sort

--- heap-sort.c
+++ heap-sort.c
@@ -4,11 +4,12 @@
  *
  * Description: This program performs heap sort on a given file with
  * comma-separated values. Below are the general steps followed by said algorithm:
- * 
- * 1. Swap max with lowest nth node
- * 2. Inactivate bound n = n-1
- * 3. Replace max with rightmost node in lowest level
- * 4. Recursively restore the heap (aka. heapify) 
+ *
+ * 1. Build a valid max heap from input array
+ * 2. Repeat until array is sorted (i.e. n is equal to 0):
+ *  2.1. Swap root (max) with rightmost node in lowest level
+ *  2.2. Inactivate bound n = n-1
+ *  2.3. Recursively restore the heap (aka. heapify)
  */
 
 #include <stdio.h>
@@ -57,7 +58,7 @@ int main(int argc, char *argv[])
 
 /**
  * Build a heap array, then recursively sort it using heap sort.
- * 
+ *
  * @param arr The array to be sorted
  * @param n Size of the array
  */
@@ -68,7 +69,8 @@ void sort(int arr[], int n)
     // Recursively sort the heap
     const int root = 0;
     int k = n;
-    while (k > 0) {
+    while (k > 0)
+    {
         swap(arr, root, k - 1);
         heapify(arr, root, --k);
     }
@@ -76,62 +78,54 @@ void sort(int arr[], int n)
 
 /**
  * Restores the heap structure from ith node downwards by swapping parent node
- * with greatest child node, recursively.
- * 
+ * with largest child node, recursively.
+ *
  * @param arr Array to be heapified
  * @param i Index of the node to be heapified
  * @param n Size of the array
  */
-void heapify(int arr[], int i, int n) {
-    int j;
-    
-    // Check if i is a leaf
-    if (LEFT(i) >= n && RIGHT(i) >= n) {
-        return;
-    }
-    
-    if (LEFT(i) < n && RIGHT(i) < n) {
-        // Heap is restored
-        if (arr[i] >= MAX(arr[LEFT(i)], arr[RIGHT(i)])) {
-            return;
-        }
-        j = arr[LEFT(i)] >= arr[RIGHT(i)] ? LEFT(i) : RIGHT(i);
-    }
-    else if (LEFT(i) < n) {
-        if (arr[i] >= arr[LEFT(i)]) {
-            return;
-        }
-        j = LEFT(i);
-    }
-    else if (RIGHT(i) < n) {
-        if (arr[i] >= arr[RIGHT(i)]) {
-            return;
-        }
-        j = RIGHT(i);
+void heapify(int arr[], int i, int n)
+{
+    int largest = i;
+
+    if (LEFT(i) < n && arr[LEFT(i)] > arr[largest])
+    {
+        largest = LEFT(i);
     }
 
-    swap(arr, i, j);
-    heapify(arr, j, n);
+    if (RIGHT(i) < n && arr[RIGHT(i)] > arr[largest])
+    {
+        largest = RIGHT(i);
+    }
+
+    if (largest != i)
+    {
+        swap(arr, i, largest);
+        heapify(arr, largest, n);
+    }
 }
 
 /**
  * Organizes a given array of integers into a heap.
- * 
+ *
  * By definition, a heap fulfills the following conditions:
  *  1. Its internal nodes have 1-2 children
  *  2. Parent node is greater than its children
  *  3. All levels (except for the leaf level) are filled
- * 
+ *
  * @param arr Array to build the heap from
  * @param n Size of the array
  */
-    void build_heap(int arr[], int n) {
-        for (int i = (n >> 1) - 1; i >= 0; i--) {
-            if (arr[i] < MAX(arr[LEFT(i)], arr[RIGHT(i)])) {
-                heapify(arr, i, n);
-            }
+void build_heap(int arr[], int n)
+{
+    for (int i = (n >> 1) - 1; i >= 0; i--)
+    {
+        if (arr[i] < MAX(arr[LEFT(i)], arr[RIGHT(i)]))
+        {
+            heapify(arr, i, n);
         }
     }
+}
 
 /**
  * It swaps two elements in an array.
