f727681bd3518345b6b53884285e0f7bcb5be223
Juan Becerra <jdb9056@nyu.edu>
2023-03-08 14:50:48 -0500

    Changes to format

--- deterministic-selection.c
+++ deterministic-selection.c
@@ -21,12 +21,12 @@
 
 #define MAX_SIZE 1000
 
-void print_array(int *, int);
-void copy(int *, int *, int, int);
-void swap(int *, int, int);
+int select(int *, int, int);
 int partition(int *, int, int);
 void sort(int *, int);
-int select(int *, int, int);
+void copy(int *, int *, int, int);
+void swap(int *, int, int);
+void print_array(int *, int);
 
 int main(int argc, char *argv[])
 {
@@ -60,35 +60,72 @@ int main(int argc, char *argv[])
 }
 
 /**
- * It copies the elements of the array arr from index start to index end into
- * the destination array.
- *
- * @param arr Array to be sorted
- * @param new Destination array
- * @param start Starting index
- * @param end Ending index
- */
-void copy(int arr[], int dest[], int start, int end)
-{
-    int i = 0;
-    while (start <= end)
-    {
-        dest[i++] = arr[start++];
-    }
-}
-
-/**
- * It swaps two elements in an array.
+ * Returns the kth smallest element in an array of numbers using the
+ * deterministic selection algorithm.
  *
  * @param arr Array of integers
- * @param i Index of the first element to swap
- * @param j Index of the second element to swap
+ * @param k Rank of the element we're looking for
+ * @param n Size of the array
+ *
+ * @return The kth smallest element in the array
  */
-void swap(int arr[], int i, int j)
+int select(int arr[], int k, int n)
 {
-    int tmp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = tmp;
+    const int m = 5; // Subset size
+
+    int S[n / m][m];
+    int R[n / m];
+    int i, j, x, r, x_i;
+
+    if (n <= 10)
+    {
+        sort(arr, n);
+        return arr[k - 1];
+    }
+
+    // Partition array into subsets of m
+    for (i = 0; i < n / m; i++)
+    {
+        for (j = 0; j < m; j++)
+        {
+            S[i][j] = arr[i * m + j];
+        }
+    }
+
+    // Copy representatives (medians) of each subset
+    for (i = 0; i < n / m; i++)
+    {
+        R[i] = select(S[i], m / 2 + 1, m);
+    }
+
+    // Find median of medians
+    x = select(R, (n / m) / 2, n / m);
+
+    // Partition array with median of medians
+    x_i = partition(arr, x, n);
+
+    // Calculate rank
+    r = x_i + 1;
+
+    int left[x_i];
+    int right[n - x_i - 1];
+
+    // Copy subsets of partition into separate arrays
+    copy(arr, left, 0, x_i - 1);
+    copy(arr, right, r, n - 1);
+
+    if (r == k)
+    {
+        return x;
+    }
+    else if (r > k)
+    {
+        return select(left, k, x_i);
+    }
+    else
+    {
+        return select(right, k - r, n - x_i - 1);
+    }
 }
 
 /**
@@ -162,72 +199,35 @@ void sort(int arr[], int n)
 }
 
 /**
- * Returns the kth smallest element in an array of numbers using the
- * deterministic selection algorithm.
+ * It copies the elements of the array arr from index start to index end into
+ * the destination array.
+ *
+ * @param arr Array to be sorted
+ * @param new Destination array
+ * @param start Starting index
+ * @param end Ending index
+ */
+void copy(int arr[], int dest[], int start, int end)
+{
+    int i = 0;
+    while (start <= end)
+    {
+        dest[i++] = arr[start++];
+    }
+}
+
+/**
+ * It swaps two elements in an array.
  *
  * @param arr Array of integers
- * @param k Rank of the element we're looking for
- * @param n Size of the array
- *
- * @return The kth smallest element in the array
+ * @param i Index of the first element to swap
+ * @param j Index of the second element to swap
  */
-int select(int arr[], int k, int n)
+void swap(int arr[], int i, int j)
 {
-    const int m = 5; // Subset size for partitioning the array
-
-    int S[n / m][m];
-    int R[n / m];
-    int i, j, x, r, x_i;
-
-    if (n <= 10)
-    {
-        sort(arr, n);
-        return arr[k - 1];
-    }
-
-    // Partition array into subsets of m
-    for (i = 0; i < n / m; i++)
-    {
-        for (j = 0; j < m; j++)
-        {
-            S[i][j] = arr[i * m + j];
-        }
-    }
-
-    // Copy representatives (medians) of each subset
-    for (i = 0; i < n / m; i++)
-    {
-        R[i] = select(S[i], m / 2 + 1, m);
-    }
-
-    // Find median of medians
-    x = select(R, (n / m) / 2, n / m);
-
-    // Partition array with median of medians
-    x_i = partition(arr, x, n);
-
-    // Calculate rank
-    r = x_i + 1;
-
-    int left[x_i];
-    int right[n - x_i - 1];
-
-    // Copy subsets of partition into separate arrays
-    copy(arr, left, 0, x_i - 1);
-    copy(arr, right, r, n - 1);
-
-    if (r == k)
-    {
-        return x;
-    }
-    else if (r > k)
-    {
-        return select(left, k, x_i);
-    }
-    else
-    {
-        return select(right, k - r, n - x_i - 1);
-    }
+    int tmp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = tmp;
 }
 
 /**
